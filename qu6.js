d6b028df3bf32b75a552ca7eaf685e9756ab7b1d
Manvendra Singh Chauhan <105724898+Manan1696@users.noreply.github.com>
2022-07-06 17:51:37 +0530

    Add files via upload

---
+++ qu6.js
@@ -0,0 +1,13 @@
+// Print the Pattern.
+// Write a program which ask user for no of lines and print a pattern using an asterik .
+
+// Note: You have to complete Print_pattern. No need to take any input.
+
+
+const Print_pattern = (N) => 
+{
+  for(let a=1; a<=N; a++){
+    console.log("*".repeat(a));
+  
+}
+};
